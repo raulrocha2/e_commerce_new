3e7b41819669067a63f2959dbc17c2997e55005a
raulrocha2 <raul.rocha2@gmail.com>
2020-04-21 00:08:23 -0300

    Add field image products/models.py

--- products/views.py
+++ products/views.py
@@ -12,10 +12,10 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
-    # def get_context_data(self):
-    # context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-    # print(context)
-    # return context
+    '''def get_context_data(self):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context  '''
 
 
 # Function Based View
