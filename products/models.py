e56013172ac8205a1d254e9161681b31aa07b344
raulrocha2 <raul.rocha2@gmail.com>
2020-04-23 16:08:50 -0300

    Creat Custom QuerySet class product manager products/models.py

--- products/models.py
+++ products/models.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+#Custom queryset
+class ProductQuerySet(models.query.QuerySet):
+    def activate(self):
+        return self.filter(active = True)
+    def featured(self):
+        return self.filter(active = True)
 
 class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using = self._db)
+    def all(self):
+        return self.get_queryset().activate()
+    def featured(self):
+        #return self.get_queryset().featured()
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
@@ -16,6 +31,9 @@ class Product(models.Model):  # product_category
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.FileField(upload_to='products/', null=True, blank=True)
+
+    featured = models.BooleanField(default = False)
+    active = models.BooleanField(default = True)
 
     objects = ProductManager()
 
