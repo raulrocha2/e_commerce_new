3034fe0d97c173ae937a2a2e798f77e2e77137fd
raulrocha2 <raul.rocha2@gmail.com>
2020-04-27 22:00:38 -0300

    modified e_commerce/urls.py products/models.py, views.py function

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from .utils import uniqui_slug_generator
+from .utils import unique_slug_generator
 from django.db.models.signals import pre_save
 #Custom queryset
 class ProductQuerySet(models.query.QuerySet):
@@ -30,13 +30,12 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model):  # product_category
     title = models.CharField(max_length=120)
-    slug  = models.SlugField(blank = True,unique = True)
+    slug = models.SlugField(blank = True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.FileField(upload_to='products/', null=True, blank=True)
-
-    featured = models.BooleanField(default = False)
-    active = models.BooleanField(default = True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     objects = ProductManager()
 
@@ -48,8 +47,10 @@ class Product(models.Model):  # product_category
     def __unicode__(self):
         return self.title
 
-    def product_pre_save_receiver(sender, instance, *args, **kwargs):
-        if not instance.slug:
-            instance.slug = uniqui_slug_generator(instance)
 
-        pre_save.connect(product_pre_save_receiveder, sender = Product)
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
