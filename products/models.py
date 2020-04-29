0841a52e6bd4dce19b3eda8cd312ccf550bfe209
raulrocha2 <raul.rocha2@gmail.com>
2020-04-29 13:30:56 -0300

    teste master

--- products/models.py
+++ products/models.py
@@ -39,8 +39,8 @@ class Product(models.Model):  # product_category
 
     objects = ProductManager()
 
-    def get_absolute_url(self):
-        return "/product/{slug}".format(slug = self.slug)
+   # def get_absolute_url(self):
+    #    return "/products/{slug}".format(slug = self.slug)
 
     # python 3
     def __str__(self):
--- products/urls.py
+++ products/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from .views import ProductListView, ProductDetailSlugView
 
 urlpatterns = [
-    url('', ProductListView.as_view()),
+    url(r'', ProductListView.as_view()),
     url('<slug:slug>/', ProductDetailSlugView.as_view())
+    #url(r'^(?P<slug>[\w_-]+)/', ProductDetailSlugView.as_view(), ),
     # url(r'^produtos/', ProductDetailView.as_view()),
     #url(r'^produtos-fbv/', product_detail_view),
     # url(r'^produtos', ProductListView.as_view()),
     #url(r'^produtos-fbv', product_list_view),
-    #url(r'^produtos/(?P<slug>[\w_-]+)/', ProductDetailSlugView.as_view(), ),
     #url(r'^destaque/', ProductFeaturedListView.as_view()),
     #url(r'^destaque/', ProductFeaturedDetailView.as_view()),
 ]
